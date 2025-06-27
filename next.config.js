af9035acbae154346cbafa0cf661b4c9463aea12
sidramesh2024 <sidramesh2024@gmail.com>
2025-06-27 15:16:54 -0400

    fixed issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
+  output: 'export',
+  trailingSlash: true,
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
